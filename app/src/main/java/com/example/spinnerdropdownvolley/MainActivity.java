7c65f52f5e601982254baab52c3b59d9627ec8a8
Christopher Panerio <christopherpaneriotup@gmail.com>
2023-03-07 03:23:49 +0800

    Spinner Dropdown Volley

---
+++ app/src/main/java/com/example/spinnerdropdownvolley/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.spinnerdropdownvolley;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] gender = {"Male","Female"};
+    AutoCompleteTextView autoCompleteTextView;
+    ArrayAdapter<String> adapterItems;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        autoCompleteTextView = findViewById(R.id.autoCompletetxt);
+        adapterItems = new ArrayAdapter<String>(this,R.layout.gender_list_item, gender);
+
+        autoCompleteTextView.setAdapter(adapterItems);
+
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String item = adapterView.getItemAtPosition(i).toString();
+                Toast.makeText(MainActivity.this, "Item: " + item, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+    }
+}
